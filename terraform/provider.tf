bc501aca18d6afb1fdc0e6cb34770a154dc7fa82
Aman Yadav <69920693+aman-yadavvv@users.noreply.github.com>
2026-02-26 18:30:23 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,12 +2,3 @@ provider "aws" {
   region = var.aws_region
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "ecs-project/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
